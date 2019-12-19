06c474461cb907455aaf2a7d4ec57975950a9372
ckubelle <ckubelle@stevens.edu>
2019-12-19 17:11:17 -0500

    Integrate better GUI, fixed bugs

--- englishToMorse.py
+++ englishToMorse.py
@@ -1,35 +1,32 @@
-import re 
 def translate(message):
-    translation = { ".-" : "A", "-..." : "B" ,  "-.-.": "C", "-.." : "D","." : "E", 
-			 "..-." : "F","--." : "G", "...." : "H", ".." : "I",  ".---" : "J",
-			 "-.-" : "K" ,".-.." : "L" , "--" : "M", "-." : "N", "---" : "O", 
-			 ".--." : "P" , "--.-" : "Q", ".-." : "R", "..." : "S", "-" : "T" ,
-			 "..-" : "U", "..-" : "V", ".--": "W", "-..-" : "X", "-.--" : "Y",
-			 "--.." : "Z", "-----": "0", ".----": "1", "..---" : "2",  "...--" : "3",
-			 "....-" : "4",".....": "5", "-....": "6", "--...": "7", "---..": "8",
-			 "----." : "9", "" : " "}
 
-    if message in translation:
-        return translation[message] + ""
+	translation = { "A" : ".-", "B" : "-...", "C" : "-.-.", "D" : "-..", "E" : ".", 
+			 "F" : "..-.", "G" : "--.", "H" : "....", "I" : "..", "J" : ".---",
+			 "K" : "-.-", "L" : ".-..", "M" : "--", "N" : "-.", "O" : "---", 
+			 "P" : ".--.", "Q" 
+			 : "--.-", "R" : ".-.", "S" : "...", "T" : "-",
+			 "U" : "..-", "V" : "..-", "W" : ".--", "X" : "-..-", "Y" : "-.--",
+			 "Z" : "--..", "0" : "-----", "1" : ".----", "2" : "..---", "3" : "...--",
+			 "4" : "....-", "5" : ".....", "6" : "-....", "7" : "--...", "8" : "---..",
+			 "9" : "----.", " " : " "}
+	if message in translation: 
+		return translation[message] + " "
 
 
-
-
+ 	
 def englishToMorse(message):
-    message = re.split(" ", message)
-    breakdown = list(message)
-    print(breakdown)
-    answer = ""
 
-    for length in breakdown: 
-        answer += translate(length)
+	message = message.upper()
+	breakdown = list(message)
+	answer = ""
 
-    
-    
-    print(answer)
 
-string = "-- -.--  -. .- -- .  .. ...  -.-. .... .-. .. ... - .. .- -."
-englishToMorse(string)
+	for length in breakdown:
+		answer += translate(length)
+
+	return answer
+
+
 
 
 
--- gui.py
+++ gui.py
@@ -1,29 +1,51 @@
 import tkinter as tk
+from tkinter import *
 import morseToEnglish
+import englishToMorse
+from tkinter import ttk
+
 root = tk.Tk() 
 root.title("morseCode Program")
 
 #returns string
 def getString():
-	englishWord = e1.get()
-	morseWord = morseToEnglish.morseToEnglish(englishWord)
-	tk.Label(root, text = " \t \t \t \t ", font = '32').grid(row=2, column = 1)
-	tk.Label(root, text = morseWord, font = '60').grid(row=2, column = 1)
+	word = e1.get()
+	if textchoice.get() == "convert to English":
+		morseWord = morseToEnglish.morseToEnglish(word)
+	if textchoice.get() == "convert to morse code":
+		morseWord = englishToMorse.englishToMorse(word)
 	
-
-label = tk.Label(root, text='morseCode program by Christian Kubelle', font = '32').grid(row=0, sticky=tk.NE)
-
-
-tk.Label(root, text="Enter a word or sentence in English: ", font = '32').grid(row=1,sticky =tk.W)
-tk.Label(root, text = "This translate to: ", font = '32').grid(row=2, column=0,sticky=tk.W)
+	tk.Label(root, text = " \t \t \t \t ").place(relx = .6, rely = .5, relheight = .07)
+	tk.Label(root, text = morseWord, font = ("Helvetica", 15)).place(relx = .6, rely = .5, relheight = .07)
 
 
+#Create initial size of program 
+root.geometry("500x375")
+
+#Title 
+label = tk.Label(root, text='morseCode program by Christian Kubelle', font = '16').place(relx = 0.2, rely = 0.05, relwidth = .6, relheight = 0.07)
+
+#What would you like to do: convert to English or convert to morse code
+label = tk.Label(root, text='Would you like to:', font = '16').place(relx = 0.01, rely = 0.18, relwidth = .3, relheight = 0.07)
+
+#Combobox
+choices = ["convert to morse code", "convert to English"]
+textchoice = StringVar()
+box = ttk.Combobox(root, values = choices ,textvariable = textchoice).place(relx = .6, rely = .18, relheight = .07)
+
+#Input
+tk.Label(root, text="Enter a word or sentence in English: ", font = '32').place(relx = .041, rely = .35, relwidth = .5 , relheight = .07)
 e1 = tk.Entry(root)
-e1.grid(row=1, column=1)	
+e1.place(relx = .6, rely = .35, relwidth = .29, relheight = .07)	
+
+#Output
+tk.Label(root, text = "This translate to: ", font = '32').place(relx = 0.01, rely = 0.5,  relwidth = .3, relheight = 0.07)
 
 
-tk.Button(root, text = 'Quit', command=root.quit, font = '32').grid(row=3, column = 0,sticky = tk.W)
-tk.Button(root, text = 'Done', command=getString, font = '32').grid(row=3, column =2,sticky = tk.E)
+
+
+tk.Button(root, text = 'Quit', command=root.quit, font = '32').place(relx = 0, rely = .9, relwidth = .2, relheight = .1)
+tk.Button(root, text = 'Translate', command=getString, font = '32').place(relx = 0.8, rely = .9, relwidth = .2, relheight = .1)
 
 
 root.mainloop()
